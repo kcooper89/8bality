c47ca33aeed75d4d7091f2e2019d042ea12d9279
Kyle Cooper <kcooper89.kc@gmail.com>
2020-04-10 17:04:01 -0400

    4/10/20 homework 3 complete

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 
 function generatePassword () {
+
+  var randomNumber=0;
+
   // Assignment Code
   var new_password="";
 
   // create an empty array called all_characters
-  var all_characters = []
+  var all_characters = [];
 
   // create an array of numbers.
   var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -22,6 +25,10 @@ function generatePassword () {
 
   var pwd_length = prompt("How long would you like your password?")
 
+   if ((parseInt(pwd_length) < 8) || (parseInt (pwd_length) > 128)){
+    alert ("Password length must be between 8 and 128 characters!");
+    return
+  }
 
   // ask (using confirm) if the user wants special characters
   var splCharacters = confirm ("Do you want $pecial Characters?");
@@ -36,80 +43,56 @@ function generatePassword () {
   var numeric = confirm ("Do you want numeric Characters?"); 
 
   // if the user did not say yes to one of those questions, terminate like above.
-  if (splCharacters = false && upperCase = false && lowerCase = false && numeric = false) {
+  if (splCharacters === false && upperCase === false && lowerCase === false && numeric === false) {
     alert ("Must contain at least one Character Type!");
+    new_password = "TRY AGAIN";
+    return new_password
     
   }
   // if the user said yes to special characters, concat special_characters array to all_characters
-  if (splCharacters = true) {
-    all_characters.concat (specialCharacter)
+  if (splCharacters === true) {
+    all_characters = all_characters.concat (specialCharacter)
   }
 
   // if the user said yes to uppercase letters, concat upper_case array to all_characters
-  if (upperCase = true) {
-    all_characters.concat (upperCases)
+  if (upperCase === true) {
+    all_characters = all_characters.concat (upperCases)
   }
 
    // if the user said yes to lowercase leters, concat lower_case array to all_characters
-  if (lowerCase = true) {
-    all_characters.concat (lowerCases)
+  if (lowerCase === true) {
+    all_characters = all_characters.concat (lowerCases)
   }
 
   // if the user said yes to numerics, concat numbers array to all_characters 
-  if (numeric = true) {
-    all_characters.concat (number)
+  if (numeric === true) {
+    all_characters = all_characters.concat (number)
   }
 
-  alert (all_characters)
   // create a for loop that executes pwd_length times.
-  //for (var = i; i < pwd_length; i++) {
-    
-  //}
+  for (var i=0; i < pwd_length; i++) {
 
-
-  //if ((parseInt(pwd_length) < 8) || (parseInt (pwd_length) > 128)){
-    //alert ("Password length must be between 8 and 128 characters!");
-    //
-  //}
-
+  // generate a random number between 0 and all_characters.length
+  randomNumber = Math.floor(Math.random() * all_characters.length);
   
-
-  // create an array of all possible characters based on user input
-  
- 
- 
-  
-
-  
-  
-  
-  
- 
-
- 
-  // [0...........................................length-1]
-  // [!,@,# ..., A,B,C,D,E...a,b,c,d,e...,1,2,3,4,5]
-
-  
-  // each iterate pick a random number between 0 and the length of all_characters
-  // append that letter to the end of new_password
-
-
-  new_password="yay"
+  // random number will represent position in the array
+  // get the letter at that index and appendto the end of new_password
+  new_password = new_password + all_characters [randomNumber];
+  }
 
   return new_password;
-}
+  }
 
-var generateBtn = document.querySelector("#generate");
+  var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
